f1add525dca2523a72cab72ecf7f50819b2d2059
Recent projects (salladdin) <40698362+alaouiii@users.noreply.github.com>
2025-03-11 14:09:55 +0000

    Update customs.js

--- upload/customs.js
+++ upload/customs.js
@@ -6,6 +6,6 @@ button.className = "button";
 
 button.addEventListener("click", function() {
   // Mở URL trong tab mới khi click
-  window.open("https://flixhive.site/cliquez-ici-pour-regarder-anime-sans-publicite/", "_blank");
+  window.open("https://9animetvtv.vercel.app/", "_blank");
 });
 document.getElementById("view-full").appendChild(button);
